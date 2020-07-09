e1c37f125d439470e9261fdbfe35e96be708e29c
chiaweilin <lin.chiawe@husky.neu.edu>
2020-07-08 23:24:27 -0700

    finish form_validator

---
+++ form_validator/script.js
@@ -0,0 +1,82 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Check require fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Check Email
+function CheckEmail(input) {
+  const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (input.value.match(mailFormat)) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid.');
+  }
+}
+
+// Check Passwords match
+function CheckPasswordsMatch(input1, input2) {
+  if (input1.value === input2.value) {
+    showSuccess(input2);
+  } else {
+    showError(input2, 'passwords do not match')
+  }
+
+}
+
+// Get fieldName
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Event listeners
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  CheckEmail(email);
+  CheckPasswordsMatch(password, password2);
+});
